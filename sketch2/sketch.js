202d1006efd6d105f5f2b96a30c7563d21357d0b
Chisako Nishimura <chisaco0403@gmail.com>
2020-05-17 18:11:43 +0900

    add skech2

---
+++ sketch2/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  
+}
+
+function draw() {
+  fill(100, frameCount*100%360,300);
+  circle(frameCount *6.6%400, frameCount *5.9 %400, frameCount%30);
+}
