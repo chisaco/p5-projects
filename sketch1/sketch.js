27a29251e85a4f99f95b8f887390b54f8b9ffe41
Chisako Nishimura <chisaco0403@gmail.com>
2020-05-17 14:50:27 +0900

    use foreach

--- sketch1/sketch.js
+++ sketch1/sketch.js
@@ -7,36 +7,37 @@ function setup() {
 function draw() {
 	background(250, 170, 239);
 
-	let red = color(191,51,50); 
-	fill(red);
-	noStroke();
-	ellipse(x, 100, 80, 80); 
-	
-	let violet = color(141,30,181	); 
-	fill(violet);
-	noStroke();
-	ellipse(x, 200, 80, 80); 
+	const items = [
+		{
+			color: color(191, 51, 50),
+			x: x,
+			y: 100
+		},
+		{
+			color: color(141, 30, 181),
+			x: x,
+			y: 200
+		},
+		{
+			color: color(94, 215, 56),
+			x: x,
+			y: 300
+		},
+		{
+			color: color(48, 138, 168),
+			x: x,
+			y: 400
+		},
+		{
+			color: color(173, 156, 50),
+			x: x,
+			y: 500
+		},
+	];
 
-	let green = color(94,215,56); 
-		
-	fill(green);
-	noStroke();
-	ellipse(x, 300, 80, 80); 
-
-	let blue = color(48,138,168	); 
-	fill(blue);
-	noStroke();
-	ellipse(x, 400, 80, 80); 
-
-	let yellow = color(173,156,50); 
-	fill(yellow);
-	noStroke();
-	ellipse(x, 500, 80, 80); 
-	//x++;
-}
-
-function draw_ellipse(color, x, y) {
-	fill(color);
-	noStroke();
-	ellipse(x, y, 80, 80); 
+	items.forEach(function (item) {
+		fill(item.color);
+		noStroke();
+		ellipse(item.x, item.y, 80, 80);
+	});
 }
